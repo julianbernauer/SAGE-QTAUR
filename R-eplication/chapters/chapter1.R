908d9ee97bc40a4cc8e33bd11666ca6dabc3761d
Julian Bernauer <julian.bernauer@googlemail.com>
2025-01-17 10:20:31 +0100

    Replication Ch1

--- R-eplication/chapters/chapter1.R
+++ R-eplication/chapters/chapter1.R
@@ -1,2 +1,19 @@
 # Replication Code for Chapter 1
 
+## Figure 1.1 
+
+# R libraries
+library(quanteda)
+library(readtext)
+library(quanteda.textplots)
+# Reading text into a data frame object named ’sahra’
+sahra <- readtext(paste0(datadir,"/bsw"),
+encoding="UTF-8")
+# Preparing the text
+sahra_dfm_trim <- dfm_remove(dfm(tokens(corpus(sahra),
+remove_punct = TRUE)),
+stopwords(language = "de"))
+# A word cloud
+textplot_wordcloud(sahra_dfm_trim,
+max_words = 100,
+color = "black")
