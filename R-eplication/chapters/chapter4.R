d79cb0308919dc47cc1ef07797107d4ea1e26b71
Julian Bernauer <julian.bernauer@googlemail.com>
2025-01-31 23:14:06 +0100

    figures ch4

--- R-eplication/chapters/chapter4.R
+++ R-eplication/chapters/chapter4.R
@@ -8,6 +8,7 @@ library(quanteda)
 library(readtext)
 library(quanteda.textplots)
 library(quanteda.textstats)
+library(gridExtra)
 
 datadir <- getwd()
 
@@ -32,7 +33,6 @@ ggtitle("Animals eating and sleeping habits") #add a title for the plot
 ggsave(filename = paste0(datadir,"/Figures/fig4_2.png"), plot = plot, width = 10, height = 8, dpi = 400)
 
 
-
 # fig 4.3
 
 #subset data
@@ -43,8 +43,9 @@ agg_data <- msleep_small %>%
     group_by(name, order) %>%
     summarise(total_sleep = sum(sleep_total))
 unique(agg_data$order) #3 order categories in the subset
+
 #create the plot
-ggplot(agg_data, aes(x = name, y = total_sleep, fill = order)) +
+plot <- ggplot(agg_data, aes(x = name, y = total_sleep, fill = order)) +
 #add 3 variables, the third through fill
 geom_bar(stat = "identity") + #bar plot
 scale_fill_manual(values = gray.colors(3)) + #you can chooseva lot of pretty colours here
@@ -55,7 +56,21 @@ theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = -1)) #because th
 ggsave(filename = paste0(datadir,"/Figures/fig4_3.png"), plot = plot, width = 10, height = 8, dpi = 400)
 
 
-# fig 4.4
+# fig 4.4 
+plot <- ggplot(agg_data, aes(x = name, y = total_sleep, fill = order)) +
+#add 3 variables, the third through fill
+geom_bar(stat = "identity") + #bar plot
+scale_fill_manual(values = gray.colors(3)) + #you can choose a lot of pretty colours here
+xlab("Animal Name") + #label the axis
+ylab("Total Sleep Hours") +
+ggtitle("Animals sleeping and eating habits") +#add main title
+theme(plot.title = element_text(family = "serif", face = "bold", colour = "blue", size = 14)) + #change font, appearance, colour and size of title
+coord_flip() +#turn from horizontal bars to vertical
+theme(legend.position="bottom") #change legend position
+ggsave(filename = paste0(datadir,"/Figures/fig4_4.png"), plot = plot, width = 10, height = 8, dpi = 400)
+
+
+# fig 4.5
 
 noNA_data <- na.omit(msleep)
 
@@ -64,7 +79,7 @@ geom_smooth(method = "lm", se = FALSE, color = "blue") + # Regression line
 xlab("Total Sleep Hours") +
 ylab("Body Weight") +
 ggtitle("Regression of Body Weight on Total Sleep Hours")
-ggsave(filename = paste0(datadir,"/Figures/fig4_4.png"), plot = plot, width = 10, height = 8, dpi = 400)
+ggsave(filename = paste0(datadir,"/Figures/fig4_5.png"), plot = plot, width = 10, height = 8, dpi = 400)
 
 
 
@@ -127,15 +142,13 @@ scales::number_format(accuracy = 0.01))
 ggsave(filename = paste0(datadir,"/Figures/fig4_6.png"), plot = plot, width = 10, height = 8, dpi = 400)
 
 
-eli_rel <- as.numeric(dfm_uk_pop2[,"elites"]
-/ntoken(dfm_uk))*100
-cor_rel <- as.numeric(dfm_uk_pop2[,"corrupt"]
-/ntoken(dfm_uk))*100
-sov_rel <- as.numeric(dfm_uk_pop2[,"sovereignty"]
-/ntoken(dfm_uk))*100
+eli_rel <- as.numeric(dfm_uk_pop2[,"elites"]/ntoken(dfm_uk))*100
+cor_rel <- as.numeric(dfm_uk_pop2[,"corrupt"]/ntoken(dfm_uk))*100
+sov_rel <- as.numeric(dfm_uk_pop2[,"sovereignty"]/ntoken(dfm_uk))*100
 party <- as.character(docvars(corpus_uk,"party"))
 relpop2 <- data.frame(party,eli_rel,cor_rel,sov_rel)
 relpop2_sort <- relpop2[order(-eli_rel),]
+
 pop_1 <- ggplot(data = relpop2_sort,
 aes(x = nrow(relpop2_sort):1, y = eli_rel)) +
 geom_point() +
@@ -143,56 +156,58 @@ coord_flip() +
 scale_x_continuous(breaks = nrow(relpop2_sort):1,
 labels = relpop2_sort$party) +
 labs(x = NULL, y = "Share of Elite References")+
-scale_y_continuous(labels =
-scales::number_format(accuracy = 0.01))
+scale_y_continuous(labels = scales::number_format(accuracy = 0.01))
+
 relpop2_sort <- relpop2[order(-cor_rel),]
-pop_2 <- ggplot(data = relpop2_sort,
-aes(x = nrow(relpop2_sort):1,
+
+pop_2 <- ggplot(data = relpop2_sort, aes(x = nrow(relpop2_sort):1,
 y = cor_rel)) +
-geom_point() +
-coord_flip() +
-scale_x_continuous(breaks =
-nrow(relpop2_sort):1,
-labels =
-relpop2_sort$party) +
-labs(x = NULL, y = "Share of Corruption Refererences")+
-scale_y_continuous(labels =
-scales::number_format(accuracy = 0.01))
-relpop2_sort <- relpop2[order(-sov_rel),]
-pop_3 <- ggplot(data = relpop2_sort,
-aes(x = nrow(relpop2_sort):1, y = sov_rel)) +
 geom_point() +
 coord_flip() +
 scale_x_continuous(breaks = nrow(relpop2_sort):1,
 labels = relpop2_sort$party) +
+labs(x = NULL, y = "Share of Corruption Refererences")+
+scale_y_continuous(labels = scales::number_format(accuracy = 0.01))
+relpop2_sort <- relpop2[order(-sov_rel),]
+
+pop_3 <- ggplot(data = relpop2_sort,
+aes(x = nrow(relpop2_sort):1, y = sov_rel)) +
+geom_point() +
+coord_flip() +
+scale_x_continuous(breaks = nrow(relpop2_sort):1, labels = relpop2_sort$party) +
 labs(x = NULL, y = "Share of Sovereignty References")+
-scale_y_continuous(labels =
-scales::number_format(accuracy = 0.01))
-grid.arrange(pop, pop_1, pop_2, pop_3, nrow = 2)
+scale_y_continuous(labels = scales::number_format(accuracy = 0.01))
+
+plot <- grid.arrange(pop, pop_1, pop_2, pop_3, nrow = 2)
+
+ggsave(filename = paste0(datadir,"/Figures/fig4_7.png"), plot = plot, width = 10, height = 8, dpi = 400)
 
 
 
 # KWIC() ANALYSIS
-elite_context <- kwic(corpus_uk,
-c("elit*", "politici*",
-"establishm*", "ruling*"),
-window = 10)
+elite_context <- kwic(corpus_uk, c("elit*", "politici*", "establishm*", "ruling*"), window = 10)
 elite_context[c(4,7,8,12,28),]
-
 
 
 # SENTIMENT ANALYSIS
 
+load(paste0(datadir,"/Data/chapter4/rap_lyrics.Rdata"))
+
+# DANN source("./code/ch4_prep.R") !!!
+
+# rap_corpus <- corpus(rap_songs, text_field="x")
+# summary(rap_corpus, n=6)
+
+# load(paste0(datadir,"/Data/chapter4/rap_songs_clean.Rda"))
+
+
 rap_dfm <- dfm(tokens(rap_corpus))
-rap_sent <- dfm_lookup(rap_dfm, dictionary = data_dictionary_
-LSD2015)
+rap_sent <- dfm_lookup(rap_dfm, dictionary = data_dictionary_LSD2015)
 rap_sent
 
-pos <- as.numeric(rap_sent[,"positive"])
-- as.numeric(rap_sent[,"neg_positive"])
+pos <- as.numeric(rap_sent[,"positive"]) - as.numeric(rap_sent[,"neg_positive"])
 
-neg <- as.numeric(rap_sent[,"negative"])
-- as.numeric(rap_sent[,"neg_negative"])
+neg <- as.numeric(rap_sent[,"negative"]) - as.numeric(rap_sent[,"neg_negative"])
 
 
 n <- as.numeric(ntoken(rap_corpus))
@@ -203,69 +218,80 @@ n <- as.numeric(ntoken(rap_corpus))
 net_tone_rap <- pos/n-neg/n
 net_tone_rap
 
-rap_lexdiv_ttr <- textstat_lexdiv(rap_dfm,
-measure = "TTR")
+
+song <- as.character(docvars(rap_corpus, "sosho"))
+plot_sent <- data.frame(net_tone_rap,song)
+
+plot <- ggplot(plot_sent, aes(x = reorder(song, net_tone_rap), y = net_tone_rap)) +
+geom_point(color = "blue", size = 2) +
+coord_flip() +
+labs(x = NULL, y = "Net Sentiment (Lexicoder)")
+
+ggsave(filename = paste0(datadir,"/Figures/fig4_8.png"), plot = plot, width = 10, height = 8, dpi = 400)
+
+
+
+rap_lexdiv_ttr <- textstat_lexdiv(rap_dfm, measure = "TTR")
 plot_ttr <- data.frame(rap_lexdiv_ttr,song)
-ggplot(plot_ttr, aes(x = reorder(song,
-TTR), y = TTR)) +
-geom_point(color = "blue", size = 2) +
-coord_flip() +
-labs(x = NULL, y = "Lexical Diversity
-(Types/Tokens)")
 
-rap_lexdiv_k <- textstat_lexdiv(rap_dfm,
-measure = "K")
-plot_k <- data.frame(rap_lexdiv_k,song)
-ggplot(plot_k, aes(x = reorder(song, K),
-y = K)) +
+plot <- ggplot(plot_ttr, aes(x = reorder(song, TTR), y = TTR)) +
 geom_point(color = "blue", size = 2) +
 coord_flip() +
-labs(x = NULL, y = "Lexical Homogeneity
-(Yule’s K)")
+labs(x = NULL, y = "Lexical Diversity (Types/Tokens)")
+
+ggsave(filename = paste0(datadir,"/Figures/fig4_9.png"), plot = plot, width = 10, height = 8, dpi = 400)
+
+
+rap_lexdiv_k <- textstat_lexdiv(rap_dfm, measure = "K")
+plot_k <- data.frame(rap_lexdiv_k,song)
+
+plot <- ggplot(plot_k, aes(x = reorder(song, K), y = K)) +
+geom_point(color = "blue", size = 2) + 
+coord_flip() +
+labs(x = NULL, y = "Lexical Homogeneity (Yule’s K)")
+
+ggsave(filename = paste0(datadir,"/Figures/fig4_10.png"), plot = plot, width = 10, height = 8, dpi = 400)
 
 cor.test(rap_lexdiv_k$K,rap_lexdiv_ttr$TTR)
 
 nscrabble("quanteda")
 
-read_rap <- textstat_readability(rap_corpus,
-measure = "ARI")
-plot_read <- data.frame(read_rap,song)
-ggplot(plot_read, aes(x = reorder(song,
-ARI), y = ARI)) +
+read_rap <- textstat_readability(rap_corpus, measure = "ARI")
+plot_read <- data.frame(read_rap,song) 
+
+plot <- ggplot(plot_read, aes(x = reorder(song, ARI), y = ARI)) +
 geom_point(color = "blue", size = 2) +
 coord_flip() +
 labs(x = NULL, y = "(Non-)Readability (ARI)")
 
+ggsave(filename = paste0(datadir,"/Figures/fig4_11.png"), plot = plot, width = 10, height = 8, dpi = 400)
+
+
 TTR <- rap_lexdiv_ttr$TTR
-plot_comp <- data.frame(net_tone_rap,
-TTR,song)
-ggplot(plot_comp, aes(x = TTR, y = net_tone_rap)) +
+plot_comp <- data.frame(net_tone_rap, TTR,song)
+
+plot <- ggplot(plot_comp, aes(x = TTR, y = net_tone_rap)) +
 geom_point(colour = "blue", size = 2) +
-labs(x = "Lexical Diversity (TTR)",
-y = "Net Sentiment (Lexicoder)") +
-geom_text(aes(label=song),
-hjust=-0.1, vjust=-0.1) +
+labs(x = "Lexical Diversity (TTR)", y = "Net Sentiment (Lexicoder)") +
+geom_text(aes(label=song), hjust=-0.1, vjust=-0.1) +
 xlim(0.3,0.6)
+
+ggsave(filename = paste0(datadir,"/Figures/fig4_12.png"), plot = plot, width = 10, height = 8, dpi = 400)
+
 
 cor.test(net_tone_rap,rap_lexdiv_ttr$TTR)
 
 
-sub_lyr <- dfm_subset(rap_dfm,
-sosho=="Message" |
-sosho=="Hustlin" |
-sosho=="Storm")
+sub_lyr <- dfm_subset(rap_dfm, sosho=="Message" | sosho=="Hustlin" | sosho=="Storm")
 sim_cor <- textstat_simil(sub_lyr)
-sim_cos <- textstat_simil(sub_lyr,
-method = "cosine")
+sim_cos <- textstat_simil(sub_lyr, method = "cosine")
 sim_cor
 sim_cos
 
 dist_euc <- textstat_dist(sub_lyr)
-dist_man <- textstat_dist(sub_lyr,
-method = "manhattan")
+dist_man <- textstat_dist(sub_lyr, method = "manhattan")
 dist_euc
 dist_man
-
 
 simvec <- as.numeric(sim_cor)
 distvec <- as.numeric(dist_euc)
@@ -273,16 +299,16 @@ simvec <- simvec[c(2,3,6)]
 distvec <- distvec[c(2,3,6)]
 so <- c("mess-hust","mess-storm","hust-storm")
 plot_sim <- data.frame(simvec,distvec,so)
-ggplot(plot_sim, aes(x = simvec, y = distvec)) +
-geom_point(colour = "blue", size = 2) +
 
-geom_smooth(mapping = aes(x = simvec,
-y = distvec), method=’lm’,
-se = F, linetype = "dashed") +
-labs(x = "Cosine Similarities (Pairwise)",
-y = "Euclidean Distances (Pairwise)") +
-geom_text(aes(label=so),hjust=-0.1,
-vjust=-0.1) +
+
+plot <- ggplot(plot_sim, aes(x = simvec, y = distvec)) +
+geom_point(colour = "blue", size = 2) +
+geom_smooth(mapping = aes(x = simvec, y = distvec), method='lm', se = F, linetype = "dashed") +
+labs(x = "Cosine Similarities (Pairwise)", y = "Euclidean Distances (Pairwise)") +
+geom_text(aes(label=so),hjust=-0.1, vjust=-0.1) +
 xlim(.45,0.75)
+
+ggsave(filename = paste0(datadir,"/Figures/fig4_13.png"), plot = plot, width = 10, height = 8, dpi = 400)
+
 
 
